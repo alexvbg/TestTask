221171fec99d7b79fc531355cd8f26f1184d4471
Aleksandr.Samarin <Aleksandr.Samarin@nexign-systems.com>
2019-01-24 13:54:34 +0300

    change handler

--- src/main/java/com/samarin/websocket/TestHandler.java
+++ src/main/java/com/samarin/websocket/TestHandler.java
@@ -2,6 +2,7 @@ package com.samarin.websocket;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -9,12 +10,21 @@ import org.springframework.web.socket.WebSocketSession;
 @Slf4j
 public class TestHandler implements WebSocketHandler {
 
+    int i = 0;
+
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         log.info("afterConnectionEstablished");
     }
 
     public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
         log.info("handleMessage" + message.getPayload().toString());
+        String textMessage = "{\"name\":\"ssid\",\"msg\":\"6259810b3881d635594a281883b4f51d\"}";
+        TextMessage message1 = new TextMessage(textMessage.getBytes());
+        if (i == 0) {
+            session.sendMessage(message1);
+            log.info("send msg - " + textMessage);
+            i++;
+        }
     }
 
     public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
--- src/test/java/com/samarin/AuthTest.java
+++ src/test/java/com/samarin/AuthTest.java
@@ -1,32 +1,23 @@
 package com.samarin;
 
 
-import com.samarin.websocket.AuthSessionClientHandler;
-import com.samarin.websocket.MySimpleStompFrameHandler;
 import com.samarin.websocket.TestHandler;
-import com.samarin.websocket.WebsocketClientBuilder;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.converter.StringMessageConverter;
-import org.springframework.messaging.simp.stomp.StompSession;
-import org.springframework.messaging.simp.stomp.StompSessionHandler;
 import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketHttpHeaders;
 import org.springframework.web.socket.WebSocketSession;
-import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
-import org.springframework.web.socket.messaging.WebSocketStompClient;
-import org.springframework.web.socket.sockjs.client.SockJsClient;
-import org.springframework.web.socket.sockjs.client.Transport;
-import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
@@ -41,18 +32,27 @@ public class AuthTest {
     }
 
     @Test
-    public void test() throws InterruptedException, ExecutionException, IOException {
+    public void test() throws InterruptedException, ExecutionException, IOException, URISyntaxException {
 
-        WebSocketClient websocketClient = new StandardWebSocketClient();
+        StandardWebSocketClient websocketClient = new StandardWebSocketClient();
+        Map<String, Object> stringObjectHashMap = new HashMap<>();
+        stringObjectHashMap.put("name", "ssid");
+        stringObjectHashMap.put("msg", "119f01df39e5a1e06da3e4b8d138b506");
+        websocketClient.setUserProperties(stringObjectHashMap);
         WebSocketHandler sessionHandler = new TestHandler();
+        WebSocketHttpHeaders webSocketHttpHeaders = new WebSocketHttpHeaders();
+        webSocketHttpHeaders.add("ssid", "d6b998e76054517ccff30d20a07b0c93");
         ListenableFuture<WebSocketSession> webSocketSessionListenableFuture =
-                websocketClient.doHandshake(sessionHandler, URL);
+                websocketClient.doHandshake(sessionHandler, webSocketHttpHeaders, new URI(URL));
 
         WebSocketSession webSocketSession = webSocketSessionListenableFuture.get();
         Assert.assertTrue(webSocketSession.isOpen());
 
+        String textMessage = "{name:\"ssid\",\"msg\":\"6259810b3881d635594a281883b4f51d\"}";
+        TextMessage message1 = new TextMessage(textMessage.getBytes());
+        webSocketSession.sendMessage(message1);
 
-        TimeUnit.SECONDS.sleep(10);
+        TimeUnit.SECONDS.sleep(30);
 //        WebSocketStompClient stompClient = new WebSocketStompClient(websocketClient);
 //        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
 //        StompSessionHandler sessionHandler = new AuthSessionClientHandler();
